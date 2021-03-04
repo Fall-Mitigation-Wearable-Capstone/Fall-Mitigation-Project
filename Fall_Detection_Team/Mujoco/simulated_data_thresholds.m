fc65631f95dc91687d9ea13664201535d3827728
ArchishaS <asinha10@ucsc.edu>
2021-03-03 23:06:20 -0800

    slip, forward, faint detection good

--- Fall_Detection_Team/Mujoco/simulated_data_thresholds.m
+++ Fall_Detection_Team/Mujoco/simulated_data_thresholds.m
@@ -4,18 +4,62 @@ clear all;
 close all;
 clc;
 
+range = 1000:1300;
+sim_data = csvread('eulers_trial5.csv');
+x = sim_data(range, :);
+time = 0:1/200:(length(x) - 1)/200;
 
-sim_data = csvread('trial4.csv')
-x = sim_data(800:1000, :)
-time = 0:1/200:(length(x) - 1)/200
+more_data = csvread('trial5.csv');
+y = more_data(range, :);
+time2 = 0:1/200:(length(y) - 1)/200;
 
 % plot(sim_data)
-
-data1 = sim_data(800:1000, 1)
-plot(time, data1*180/pi)
+figure(1)
+data1 = sim_data(range, 1);
+plot(time, data1)
 hold on
-data2 = sim_data(800:1000, 2)
-plot(time, data2*180/pi)
-data3 = sim_data(800:1000, 3)
-plot(time, data3*180/pi)
+data2 = sim_data(range, 2);
+plot(time, data2)
+data3 = sim_data(range, 3);
+plot(time, data3)
+legend("yaw", "pitch", "roll")
+
+figure(2)
+data4 = more_data(range, 1);
+plot(time2, data4*180/pi)
+hold on
+data5 = more_data(range, 2);
+plot(time2, data5*180/pi)
+data6 = more_data(range, 3);
+plot(time2, data6*180/pi)
+legend("x", "y", "z")
+
+range2 = 1500:2100;
+% sim_data = csvread('eulers_trial5.csv')
+x = sim_data(range2, :);
+time = 0:1/200:(length(x) - 1)/200;
+
+% more_data = csvread('trial5.csv')
+y = more_data(range2, :);
+time2 = 0:1/200:(length(y) - 1)/200;
+
+% plot(sim_data)
+figure(3)
+data1 = sim_data(range2, 1);
+plot(time, data1)
+hold on
+data2 = sim_data(range2, 2);
+plot(time, data2)
+data3 = sim_data(range2, 3);
+plot(time, data3)
+legend("yaw", "pitch", "roll")
+
+figure(4)
+data4 = more_data(range2, 1);
+plot(time2, data4*180/pi)
+hold on
+data5 = more_data(range2, 2);
+plot(time2, data5*180/pi)
+data6 = more_data(range2, 3);
+plot(time2, data6*180/pi)
 legend("x", "y", "z")
